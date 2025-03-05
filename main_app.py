7e57b6d4f3ae06e38940c1e36c301883dfb079a5
kensei0527 <kensei268@icloud.com>
2025-03-04 22:52:58 -0700

    Ignore large model file

--- main_app.py
+++ main_app.py
@@ -996,7 +996,7 @@ if __name__ == "__main__":
 
     # Flask起動
     try:
-        app.run(host="0.0.0.0", port=8080, debug=True)
+        app.run(host="0.0.0.0", port=8000, debug=True)
     finally:
         is_running = False
         t.join()
